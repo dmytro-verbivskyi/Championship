73bed151b57def744f14ecd8b0429202d810730e
Dima Home <dverbovskiy@gmail.com>
2013-03-29 01:00:29 +0200

    init

---
+++ src/main/java/Championship/App.java
@@ -0,0 +1,13 @@
+package Championship;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
